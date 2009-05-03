9dea5493358be1e93e31c7fb0ee61b4585297ce0
rjensen <wino@jentronics.com>
2009-05-03 11:18:24 -0600

    Reasonable default for the airfoil

--- main.c
+++ main.c
@@ -17,8 +17,10 @@ int main(int argc, char *argv[])
     if (argc > 1) {
       ReadDatcom(argv[1], &ac);
       PrintAC(&ac);    
-fprintf(stderr, "NACA_DESCR = %s\n", ac.wingfoil.NACA_DESCR);
-      NacaFoil(ac.wingfoil.NACA_DESCR, &wingfoil);
+	if(ac.wingfoil.NACA_DESCR)
+	      NacaFoil(ac.wingfoil.NACA_DESCR, &wingfoil);
+	else
+	      NacaFoil("NACA-W-4-2414", &wingfoil);
 
       InitAC(stdout, 2);
       WriteWing(stdout, &ac.wing, &wingfoil, &ac.synths);
